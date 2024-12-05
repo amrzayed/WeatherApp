584bc9047deb98430fea94ff2e3c90422b30d69b
Amr AbdelHalem <288+amr-abdelhalem@users.noreply.github.onebank.local>
2024-12-05 10:04:13 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by amr zayed on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
